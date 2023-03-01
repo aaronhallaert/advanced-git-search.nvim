d9eaadf0a9d532d9ff817e21c73c0a3afc84a635
Aaron Hallaert <hallaertaaron@gmail.com>
2023-03-01 22:59:54 +0100

    Update picker titles

--- lua/advanced_git_search/init.lua
+++ lua/advanced_git_search/init.lua
@@ -9,57 +9,18 @@ local finders = require("telescope.finders")
 
 M = {}
 
---- Opens a Telescope window with all files changed on the current branch
---- Only committed changes will be displayed
----
---- <CR> to open the file
---- <C-d> to open a diff with the base branch
--- M.changed_on_branch = function()
---     pickers
---         .new({
---             results_title = "Modified on current branch",
---             finder = finders.new_oneshot_job(vim.tbl_flatten({
---                 "bash",
---                 "git-branch-modified.sh",
---                 "list",
---             })),
---             sorter = sorters.get_fuzzy_file(),
---             previewer = previewers.new_termopen_previewer({
---                 get_command = function(entry, _)
---                     return {
---                         "bash",
---                         "git-branch-modified.sh",
---                         "diff",
---                         entry.value,
---                     }
---                 end,
---             }),
---             attach_mappings = function(_, map)
---                 map("i", "<C-d>", function(prompt_bufnr)
---                     actions.close(prompt_bufnr)
---                     local selection = action_state.get_selected_entry()
---                     local file = selection.value
---                     vim.cmd(":edit " .. file)
---                     gu.open_diff_view(gu.base_branch())
---                 end)
---
---                 return true
---             end,
---         })
---         :find()
--- end
-
 --- Opens a Telescope window with a list of local branches
 ---
 --- <CR> opens a diff for the current file with the selected branch
-M.diff_file_branch = function()
+M.diff_branch_file = function()
     -- local previewers = require('telescope.previewers')
     local current_branch = vim.fn.system("git branch --show-current")
     current_branch = string.gsub(current_branch, "\n", "")
     local file_name = vim.fn.expand("%:~:.")
     pickers
         .new({
-            results_title = "Local branches :: " .. current_branch,
+            results_title = "Local branches :: *" .. current_branch,
+            prompt_title = "Branch name",
             finder = finders.new_oneshot_job({
                 "git",
                 "branch",
@@ -94,11 +55,12 @@ M.diff_file_branch = function()
         :find()
 end
 
---- Opens a Telescope window with a list of previous commit logs with respect to selected lines
+--- Opens a Telescope window with a list of previous commit logs
+--- with respect to selected lines
 ---
 --- <CR> opens a diff for the current file with the selected commit
 --- <C-o> opens a the selected commit in the browser
-M.diff_file_log = function()
+M.diff_commit_line = function()
     local file_name = vim.fn.expand("%")
     local relative_file_name = vim.fn.expand("%:~:.")
     local s_start = vim.fn.getpos("'<")[2]
@@ -107,7 +69,8 @@ M.diff_file_log = function()
     -- git log -L741,751:'app/models/patients/patient.rb' --format='%C(auto)%h \t %as \t %C(green)%an _ %Creset %s'
     pickers
         .new({
-            results_title = "Commits on selected lines, grep message, <CR> to compare to current",
+            results_title = "Commits that affected the selected lines",
+            prompt_title = "Case-sensitive commit message",
             finder = gu.git_log_grepper_on_location(relative_file_name, s_start, s_end),
             -- finder = finders.new_oneshot_job({'git', 'log', location}),
             previewer = previewers.new_termopen_previewer({
@@ -145,14 +108,21 @@ M.diff_file_log = function()
         :find()
 end
 
-M.search_git_log = function()
+--- Opens a Telescope window with a list of previous commits.
+--- Query is used to filter the results based on the
+--- content of the commit (added or removed text).
+---
+--- <CR> opens a diff for the current file with the selected commit
+--- <C-o> opens a the selected commit in the browser
+M.search_log_content = function()
     -- local file_name = vim.fn.expand("%")
     -- local relative_file_name = vim.fn.expand("%:~:.")
 
     -- git log -L741,751:'app/models/patients/patient.rb' --format='%C(auto)%h \t %as \t %C(green)%an _ %Creset %s'
     pickers
         .new({
-            results_title = "Search git log",
+            results_title = "Commits",
+            prompt_title = "Git log content (added, removed or updated text)",
             finder = gu.git_log_grepper_on_content(),
             -- finder = finders.new_oneshot_job({'git', 'log', location}),
             previewer = previewers.new_termopen_previewer({
@@ -193,12 +163,13 @@ end
 -- <CR> Opens a diff of the current file with the selected commit
 -- <C-e> Opens an entire git diff of the selected commit
 -- <C-o> Open the selected commit in the browser
-M.diff_file_commit = function()
+M.diff_commit_file = function()
     local filename = vim.fn.expand("%:~:.")
 
     pickers
         .new({
-            results_title = "Commits history for entire file",
+            results_title = "Commits that affected this file (renamed files included)",
+            prompt_title = "Case-sensitive commit message",
             finder = gu.git_log_grepper_on_file(filename),
             previewer = previewers.new_termopen_previewer({
                 get_command = function(entry)
@@ -288,12 +259,11 @@ M.checkout_reflog = function()
 end
 
 local git_functions = {
-    { value = "Find in log", func = M.search_git_log },
-    { value = "Diff file with branch", func = M.diff_file_branch },
-    { value = "Diff file with previous commit", func = M.diff_file_commit },
-    { value = "Diff file with line history", func = M.diff_file_log },
+    { value = "Find in log content", func = M.diff_git_log_content },
+    { value = "Diff file with branch", func = M.diff_branch_file },
+    { value = "Diff file with previous commit", func = M.diff_commit_file },
+    { value = "Diff file with selected line history", func = M.diff_commit_line },
     { value = "Checkout from reflog", func = M.checkout_reflog },
-    -- { value = "Show changes on branch", func = M.changed_on_branch },
 }
 
 local function map_item(git_functions_table, f)
@@ -313,6 +283,7 @@ local function execute_git_function(value)
     end
 end
 
+--- Opens all a selector for all advanced git search functions
 M.show_custom_functions = function()
     pickers
         .new({
--- lua/telescope/_extensions/advanced_git_search.lua
+++ lua/telescope/_extensions/advanced_git_search.lua
@@ -2,11 +2,11 @@ local func = require("advanced_git_search")
 
 return require("telescope").register_extension({
     exports = {
-        diff_file_branch = func.diff_file_branch,
-        diff_file_log = func.diff_file_log,
-        search_git_log = func.search_git_log,
-        diff_file_commit = func.diff_file_commit,
         checkout_reflog = func.checkout_reflog,
+        diff_branch_file = func.diff_branch_file,
+        diff_commit_file = func.diff_commit_file,
+        diff_commit_line = func.diff_commit_line,
+        search_log_content = func.search_log_content,
         show_custom_functions = func.show_custom_functions,
     },
 })
