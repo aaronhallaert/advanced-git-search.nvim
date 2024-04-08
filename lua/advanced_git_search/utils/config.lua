ac8a7c514db868598df654d870a219ff53f7d693
marcoSven <me@marcosven.com>
2024-04-08 06:27:38 -0700

    feat: add keymap configuration for Telescope picker
    Signed-off-by: marcoSven <me@marcosven.com>

--- lua/advanced_git_search/utils/config.lua
+++ lua/advanced_git_search/utils/config.lua
@@ -37,8 +37,7 @@ local function get_keymaps()
         toggle_date_author = "<C-w>",
         open_commit_in_browser = "<C-o>",
         copy_commit_hash = "<C-y>",
-        -- TODO: add new option to telescope picker and README. Change value to "<C-e>"
-        show_entire_commit = "ctrl-e",
+        show_entire_commit = "<C-e>",
     }
     keymaps = vim.tbl_extend("force", keymaps, config["keymaps"] or {})
 
